2b76cf5b733524539fe52cfee4c707b075831654
mlarnick <31775845+mlarnick@users.noreply.github.com>
2017-12-08 10:23:08 -0500

    add pg 502

--- Prototype 3 - Space SHMUP/Assets/__Scripts/Hero.cs
+++ Prototype 3 - Space SHMUP/Assets/__Scripts/Hero.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static public Hero S;
+
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    public float shieldLevel = 1;
+
+    public bool __________________;
+
+    public Bounds bounds;
+
+    void Awake()
+    {
+        S = this;
+        bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
 }
